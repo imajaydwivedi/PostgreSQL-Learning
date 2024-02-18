7599329d3af97c2a0926af00dcb38dda9dfab5a6
Ajay Dwivedi <ajay.dwivedi2007@gmail.com>
2024-02-18 18:02:37 +0530

    updated commands

--- Setup-Configuration/General.sh
+++ Setup-Configuration/General.sh
@@ -1,6 +1,8 @@
 # Location of postgresql.conf and pg_hba.conf on an Ubuntu server
+# Where does PostgreSQL store configuration/conf files?
   # https://serverfault.com/questions/152942/location-of-postgresql-conf-and-pg-hba-conf-on-an-ubuntu-server
   # https://serverfault.com/a/877213
+  # https://stackoverflow.com/a/3603162
 export PGPASSWORD='<password>'
 > psql -U postgres
 =# show hba_file;
@@ -11,6 +13,14 @@ export PGPASSWORD='<password>'
   # https://askubuntu.com/a/594002
 sudo -u postgres psql
 ALTER USER postgres PASSWORD 'newpassword';
+
+# Locate config file within connecting to Postgres
+(base) saanvi@ryzen9:~/Github/PostgreSQL-Learning$ sudo -u postgres psql -c 'SHOW config_file'
+               config_file               
+-----------------------------------------
+ /etc/postgresql/14/main/postgresql.conf
+(1 row)
+
 
 
 # connect to PostgreSQL server: FATAL: no pg_hba.conf entry for host
@@ -26,8 +36,7 @@ host  all  all 0.0.0.0/0 scram-sha-256
 Restart Postgresql after adding this with service postgresql restart or the equivalent command for your setup. For brew, brew services restart postgresql
 
 
-# Getting error: Peer authentication failed for user "postgres", when trying to get pgsql working with rails
-  # https://stackoverflow.com/a/18664239/4449743
-# Change following line to 
-#local   all             postgres                                peer
-local   all             postgres                                md5
+# Connect to Postgres using password
+export PGPASSWORD='<password>'
+> psql -h localhost -U postgres
+
--- Setup-Configuration/restore-sample-dbs.sh
+++ Setup-Configuration/restore-sample-dbs.sh
@@ -1,4 +1,5 @@
 export PGPASSWORD='<password>';
 
+# Restore Sample Database from *.tar File
 /PostgreSQL/14/bin> pg_restore -h localhost -d dvdrental /home/saanvi/Study-Zone/DBMS/PostgreSQL/dvdrental.tar
 
