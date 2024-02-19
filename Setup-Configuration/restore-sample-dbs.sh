ca88bd8e698ae3d0bccef1d2ca300568a841da6c
Ajay Dwivedi <ajay.dwivedi2007@gmail.com>
2024-02-19 10:24:56 +0530

    Added backup command

--- Setup-Configuration/restore-sample-dbs.sh
+++ Setup-Configuration/restore-sample-dbs.sh
@@ -1,5 +1,8 @@
 export PGPASSWORD='<password>';
 
 # Restore Sample Database from *.tar File
-/PostgreSQL/14/bin> pg_restore -h localhost -d dvdrental /home/saanvi/Study-Zone/DBMS/PostgreSQL/dvdrental.tar
+/PostgreSQL/14/bin> pg_restore -h localhost -d dvdrental /stale-storage/Softwares/PostgreSQL/PostgreSQL-Sample-Dbs/dvdrental.tar
+
+# Backup database
+(base) saanvi@ryzen9:~$ pg_dump -U postgres -h localhost -d forumdb -F tar -f /stale-storage/Softwares/PostgreSQL/PostgreSQL-Sample-Dbs/forumdb.tar
 
