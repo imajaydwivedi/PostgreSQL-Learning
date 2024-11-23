f079a71d1a4e60c8de0a3950bebe2b2aa672d862
Ajay Kumar Dwivedi <ajay.dwivedi2007@gmail.com>
2024-11-23 07:00:03 +0530

    added query to compare performance b/w ms & pg

--- stackoverflow/MSSQL-vs-PG--IN-Clause.sql
+++ stackoverflow/MSSQL-vs-PG--IN-Clause.sql
@@ -21,10 +21,38 @@ First for DisplayName = 'Ajay' & Location = 'India'
 Second for DisplayName = 'David' & Location = 'India'
 */
 
-explain (analyze, buffers, costs, verbose)
-select *
-from users u
-where u.displayname in ('ajay','david')
-and u.location = 'india'
+/* ********************************************************************************************************************* */
+create index users__display_name__location on users (displayname, location);
 
+explain (analyze, buffers, costs, verbose)
+SELECT *
+FROM users u
+WHERE u.displayname in ('Ajay','David')
+  AND u.location = 'India'
+-- Index not used
+
+explain (analyze, buffers, costs, verbose)
+SELECT *
+FROM users u
+WHERE u.displayname ILIKE ANY (ARRAY['Ajay', 'David'])
+  AND u.location ILIKE 'India';
+-- Index not used
+
+create index users__display_name_lowercase__location_lowercase on users (lower(displayname), lower(location));
+
+explain (analyze, buffers, costs, verbose)
+SELECT *
+FROM users u
+WHERE lower(u.displayname) in (lower('Ajay'), lower('David'))
+  AND lower(u.location) = lower('India');
+-- Bitmap Index Scan 
+
+
+/* ***************************** CONCLUSION ********************************* */
+
+SQLServer engine took approx 10 ms.
+
+PG engine took 1 ms with index users__display_name__location with CASE SENSITIVE comparision.
+PG engine took 5 seconds with index users__display_name__location with CASE INSENSITIVE comparision.
+PG engine took 1 ms with index users__display_name_lowercase__location_lowercase.
 
