f053e3d2bc7ae73b1e7ec6e028019c8271580c9b
Ajay Kumar Dwivedi <ajay.dwivedi2007@gmail.com>
2024-12-06 22:26:49 +0530

    updated queries

--- Queries/General-Queries.sql
+++ Queries/General-Queries.sql
@@ -24,11 +24,6 @@ select * from pg_stat_activity where query not like '<%';
 -- all databases and their sizes
 select * from pg_user;
 
--- all tables and their size, with/without indexes
-select datname, pg_size_pretty(pg_database_size(datname))
-from pg_database
-order by pg_database_size(datname) desc;
-
 -- cache hit rates (should not be less than 0.99)
 SELECT sum(heap_blks_read) as heap_read, sum(heap_blks_hit)  as heap_hit, (sum(heap_blks_hit) - sum(heap_blks_read)) / sum(heap_blks_hit) as ratio
 FROM pg_statio_user_tables;
@@ -136,32 +131,32 @@ join pg_catalog.pg_namespace on relnamespace = pg_catalog.pg_namespace.oid
 where pg_catalog.pg_namespace.nspname not in ('pg_catalog','pg_toast','information_schema')
 order by relkind desc, relname desc;
 
-
 /* Get Indexes & Attributes */
 SELECT
-  --U.usename                AS user_name,
-  --ns.nspname               AS schema_name,
-  idx.indrelid :: REGCLASS AS table_name,
-  i.relname                AS index_name,
-  idx.indisunique          AS is_unique,
-  idx.indisprimary         AS is_primary,
-  am.amname                AS index_type,
-  --idx.indkey,
-       ARRAY(
-           SELECT pg_get_indexdef(idx.indexrelid, k + 1, TRUE)
-           FROM
-             generate_subscripts(idx.indkey, 1) AS k
-           ORDER BY k
-       ) AS index_keys,
-  (idx.indexprs IS NOT NULL) OR (idx.indkey::int[] @> array[0]) AS is_functional,
-  idx.indpred IS NOT NULL AS is_partial
+        --U.usename                AS user_name,
+        ns.nspname               AS schema_name,
+        idx.indrelid :: REGCLASS AS table_name,
+        --t.relname                 as table_name2,
+        i.relname                AS index_name,
+        idx.indisunique          AS is_unique,
+        idx.indisprimary         AS is_primary,
+        am.amname                AS index_type,
+        --idx.indkey,
+            ARRAY(
+                SELECT pg_get_indexdef(idx.indexrelid, k + 1, TRUE)
+                FROM
+                    generate_subscripts(idx.indkey, 1) AS k
+                ORDER BY k
+            ) AS index_keys,
+        (idx.indexprs IS NOT NULL) OR (idx.indkey::int[] @> array[0]) AS is_functional,
+        idx.indpred IS NOT NULL AS is_partial
 FROM pg_index AS idx
-  JOIN pg_class AS i
-    ON i.oid = idx.indexrelid
-  JOIN pg_am AS am
-    ON i.relam = am.oid
-  JOIN pg_namespace AS NS ON i.relnamespace = NS.OID
-  JOIN pg_user AS U ON i.relowner = U.usesysid
+JOIN pg_class AS i ON i.oid = idx.indexrelid
+JOIN pg_class AS t on t.oid = idx.indrelid
+JOIN pg_am AS am ON i.relam = am.oid
+JOIN pg_namespace AS NS ON i.relnamespace = NS.OID
+JOIN pg_user AS U ON i.relowner = U.usesysid
 WHERE 1=1
 and nspname = 'public' -- Excluding system tables
+and t.relname in ('posts') 
 LIMIT 20;
---
+++ Setup-Configuration/Podman-commands.sh
@@ -0,0 +1,12 @@
+## Use volumes for Persistent Data
+
+# create a volume
+podman volume create postgres_data
+
+# Attach the volume to the container
+podman run --name postgres \
+  -e POSTGRES_USER=postgres \
+  -e POSTGRES_PASSWORD='DummyPassword' \
+  -p 5432:5432 \
+  -v postgres_data:/var/lib/postgresql/data \
+  -d postgres
