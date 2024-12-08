6aca6ab02482712b8f0d063ef1278a254991ea87
Ajay Kumar Dwivedi <ajay.dwivedi2007@gmail.com>
2024-12-08 21:33:49 +0530

    updated psql prompt code

--- Setup-Configuration/psql-prompt.sh
+++ Setup-Configuration/psql-prompt.sh
@@ -1,6 +1,9 @@
 # Update psql prompt to include host name
+    # https://gist.github.com/viniciusdaniel/53a98cbb1d8cac1bb473da23f5708836#foreground-text
+    # https://www.postgresql.org/docs/current/app-psql.html#APP-PSQL-PROMPTING
 
 (base) saanvi@ryzen9:PostgreSQL-Learning$ nano ~/.psqlrc
     -------------------
-    \set PROMPT1 '%[%033[96m%]%n@%[%033[95m%]%m %[%033[92m%]%/%[%033[0m%]%R%# '
+    \set PROMPT1 '%[%033[96m%]%n@%[%033[95m%]%m %[%033[39m%][%`date +%H:%M:%S`] %[%033[92m%](%/) \n%[%033[1;33m%]%R%#%]%[%033[0;39m%]%] '
+    \set PROMPT2 '%[%033[1;33m%]%R%#%]%[%033[0;39m%]%] '
     -------------------
