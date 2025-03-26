13e5035eed12d217709cc9e9d742291f503a1d3a
Ajay Kumar Dwivedi <ajay.dwivedi2007@gmail.com>
2025-03-26 08:16:01 +0530

    updated mssql commands

--- Admin-Queries/config-changes.sh
+++ Admin-Queries/config-changes.sh
@@ -10,3 +10,8 @@ select name, setting, context, pending_restart from pg_settings where name = 'au
 select pg_reload_conf();
 
 select name, setting, context, pending_restart from pg_settings where name = 'autovacuum';
+
+
+autovacuum = 'on'
+max_parallel_workers = '4'
+max_parallel_workers_per_gather = '2'
